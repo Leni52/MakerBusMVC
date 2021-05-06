afcd2c4a2882a4c1d6969f492cae14baa2e9f7e4
Lenche <lencegeorgieva@yahoo.com>
2021-05-06 08:35:01 +0200

    Changes

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakerBusMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Route()
+        {
+            return View();
+        }
+
+        public IActionResult Toys()
+        {
+            return View();
+        }
+
+        public IActionResult Timetable()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MakerBusMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
